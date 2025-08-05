dce14c616f16255367fcf817feefa1f5645d5f3f
Daeng <89900251+yoondaeng@users.noreply.github.com>
2025-08-05 22:34:08 +0900

    Modify BOJ_17070 파이프 옮기기 1

--- WEEK_03/BOJ_17070/yoonji.py
+++ WEEK_03/BOJ_17070/yoonji.py
@@ -7,30 +7,29 @@ for i in range(N):
 dr = [0, 1, 1]
 dc = [1, 0, 1]
 
-count = 0
 def dfs(r, c, d):
-    global count
     if r == N - 1 and c == N - 1:  # 도착지점
-        count += 1
-        return
-                
+        return 1
+    
+    count = 0                
     if d == 0:  # 가로
         if c + 1 < N and arr[r][c + 1] == 0:
-            dfs(r, c + 1, 0)
+            count += dfs(r, c + 1, 0)
         if r + 1 < N and c + 1 < N and arr[r + 1][c] == 0 and arr[r][c + 1] == 0 and arr[r + 1][c + 1] == 0:
-            dfs(r + 1, c + 1, 2)
+            count += dfs(r + 1, c + 1, 2)
     elif d == 1:  # 세로
         if r + 1 < N and arr[r + 1][c] == 0:
-            dfs(r + 1, c, 1)
+            count += dfs(r + 1, c, 1)
         if r + 1 < N and c + 1 < N and arr[r][c + 1] == 0 and arr[r + 1][c] == 0 and arr[r + 1][c + 1] == 0:
-            dfs(r + 1, c + 1, 2)
+            count += dfs(r + 1, c + 1, 2)
     else:  # 대각선
         if c + 1 < N and arr[r][c + 1] == 0:
-            dfs(r, c + 1, 0)
+            count += dfs(r, c + 1, 0)
         if r + 1 < N and arr[r + 1][c] == 0:
-            dfs(r + 1, c, 1)
+            count += dfs(r + 1, c, 1)
         if r + 1 < N and c + 1 < N and arr[r + 1][c] == 0 and arr[r][c + 1] == 0 and arr[r + 1][c + 1] == 0:
-            dfs(r + 1, c + 1, 2)
+            count += dfs(r + 1, c + 1, 2)
+
+    return count
   
-dfs(0, 1, 0)
-print(count)
+print(dfs(0, 1, 0))
