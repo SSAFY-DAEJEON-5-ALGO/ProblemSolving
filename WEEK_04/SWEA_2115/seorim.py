8100ad0695ae69dc098a270e8e6bc773eb4596ec
srcho01 <seorim5306@gmail.com>
2025-08-13 15:14:37 +0900

    Modify SWEA_2115 벌꿀채취

--- WEEK_04/SWEA_2115/seorim.py
+++ WEEK_04/SWEA_2115/seorim.py
@@ -38,26 +38,18 @@ for test_case in range(1, T+1):
     idx2 = 0
     for i in range(1, len(max_profit)):
         val2, x2, y2 = max_profit[i]
-        if x1 == x2 and abs(y1-y2) < m:
-            continue
-        
-        idx2 = i
-        break
+        if not (x1 == x2 and abs(y1-y2) < m):
+            idx2 = i
+            break
         
     ans = val1 + val2
     
-    idx1 += 1
-    idx2 -= 1
-    while idx1 < idx2:
-        val1, x1, y1 = max_profit[idx1]
-        val2, x2, y2 = max_profit[idx2]
-        
-        idx1 += 1
-        idx2 -= 1
-        
-        if x1 == x2 and abs(y1-y2) < m:
-            continue
-        
-        ans = max(ans, val1 + val2)
+    for i in range(idx1+1, idx2):
+        for j in range(i+1, idx2):
+            val1, x1, y1 = max_profit[i]
+            val2, x2, y2 = max_profit[j]
+            
+            if not (x1 == x2 and abs(y1-y2) < m):
+                ans = max(ans, val1 + val2)
         
     print(f"#{test_case} {ans}")
